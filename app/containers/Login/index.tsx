64bf8a9defb20f32471934ad3939f17d20ce92f4
Spielberg <javiersanchezostiz@gmail.com>
2024-10-29 12:02:21 +0100

    refactor

--- app/containers/Login/index.tsx
+++ app/containers/Login/index.tsx
@@ -1,54 +1,44 @@
+import * as React from 'react';
 import {
   View,
-  Text,
   TouchableOpacity,
   KeyboardAvoidingView,
   Keyboard,
   TouchableWithoutFeedback,
 } from 'react-native';
-import React, {useState} from 'react';
-import colors from '../../assets/colors';
-import {hp, normalize, wp} from '../../helper/responsiveScreen';
-import {Button, FontText, Input} from '../../components';
-import {appConstant} from '../../helper/appConstant';
-import SvgIcons from '../../assets/svgIcons';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+
+import { appConstant } from '@/app/helper/appConstant';
+import { Button, FontText } from '@/app/components';
+import { hp, normalize, wp } from '@/app/helper/responsiveScreen';
+import colors from '@/app/assets/colors';
+import CustomInput from '@/app/components/Input/CustomInput';
+import CustomPassword from '@/app/components/Input/CustomPassword';
+import SvgIcons from '@/app/assets/svgIcons';
+
 import s from './style';
-import { RouteName } from '../../helper/routeName';
-import { emailRegex } from '../../helper/constantData';
+
+const cfg = {
+  validationSchema: Yup.object().shape({
+    email: Yup.string().email('Invalid email').required('Email is required'),
+    password: Yup.string().required('Password is required'),
+    rememberMe: Yup.boolean(),
+  }),
+}
 
 const Login = ({navigation}: any) => {
-  const passwordRef: any = React.useRef();
-  const [email, setEmail] = useState('');
-  const [emailError, setEmailError] = useState(false);
-  const [password, setPassword] = useState('');
-  const [passwordError, setPasswordError] = useState(false);
-  const [showPassword, setShowPassword] = useState(false);
-  const [rememberPassword, setRememberPassword] = useState(false);
-  const [emailFocus, setEmailFocus] = useState(false);
-  const [passwordFocus, setPasswordFocus] = useState(false);
-
   React.useLayoutEffect(() => {
     navigation.setOptions({
-      headerTitle: `${appConstant.logIn}`,
+      headerTitle: appConstant.logIn,
       headerTitleStyle: s.headerTitle,
       headerLeft: () => (
-        <TouchableOpacity
-          style={s.backIcon}>
+        <TouchableOpacity style={s.backIcon}>
           <SvgIcons.BackIcon />
         </TouchableOpacity>
       ),
     });
   }, [navigation]);
-
-  const onLoginPress = async () => {
-    if (email === '' || !emailRegex.test(email)) {
-      setEmailError(true);
-    } else if (password === '' || password.length < 6) {
-      setPasswordError(true);
-    } else {
-      navigation.replace(RouteName.bottomTab);
-    }
-  };
 
   return (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
@@ -73,133 +63,75 @@ const Login = ({navigation}: any) => {
                 {appConstant.welcomeDescription}
               </FontText>
             </View>
-            <View style={[s.textInputContainer]}>
-              <Input
-                withTitle
-                title={appConstant.email}
-                titlecolor={colors.black}
-                autoCapitalize={'none'}
-                titleSize={normalize(14)}
-                placeholder={'Enter Email'}
-                value={email}
-                onChangeText={(text: any) => {
-                  setEmail(text.trim());
-                }}
-                placeholderTextColor={colors.gray}
-                keyboardType={'default'}
-                returnKeyType={'next'}
-                fontName={'inter-medium'}
-                fontSize={normalize(14)}
-                color={'blue'}
-                inputStyle={s.textInput}
-                onFocus={() => setEmailFocus(!emailFocus)}
-                onBlur={() => setEmailFocus(!emailFocus)}
-                onSubmit={() => passwordRef?.current.focus()}
-                style={{
-                  borderColor:
-                    emailFocus === true
-                      ? colors.primary
-                      : colors.grayBorderColor,
-
-                  height: hp(6),
-                }}
-              />
-              {emailError === true && !emailRegex.test(email) ? (
-                <FontText color={colors.red} size={normalize(12)} pLeft={wp(2)}>
-                  {appConstant.emailError}
-                </FontText>
-              ) : (
-                <FontText size={normalize(12)}> </FontText>
+            <Formik
+              initialValues={{
+                email: 'test@mail.com',
+                password: '123456',
+                rememberMe: false,
+              }}
+              validationSchema={cfg.validationSchema}
+              onSubmit={(values) => console.log(values)}
+            >
+              {({ handleChange, handleBlur, handleSubmit, setFieldValue, values, errors }) => (
+                <React.Fragment>
+                  <CustomInput
+                    title={appConstant.email}
+                    placeHolder="Enter Email"
+                    name="email"
+                    handleChange={handleChange}
+                    handleBlur={handleBlur}
+                    value={values.email}
+                    error={errors.email}
+                  />
+                  <CustomPassword
+                    title={appConstant.password}
+                    placeHolder="Enter Password"
+                    name="password"
+                    handleChange={handleChange}
+                    handleBlur={handleBlur}
+                    value={values.password}
+                    error={errors.password}
+                    viewStyle={{ marginTop: hp(-2) }}
+                  />
+                  <View style={s.forgotPassContainer}>
+                    <TouchableOpacity
+                      style={s.rememberView}
+                      onPress={() => setFieldValue('rememberMe', !values.rememberMe)}>
+                      {values.rememberMe ? (
+                        <SvgIcons.EmptyCheckbox />
+                      ) : (
+                        <SvgIcons.CheckboxIcon />
+                      )}
+                      <FontText
+                        name={'inter-regular'}
+                        size={normalize(13)}
+                        color={colors.darkGray}
+                        pLeft={wp(2)}>
+                        {appConstant.rememberMe}
+                      </FontText>
+                    </TouchableOpacity>
+                    <FontText
+                      name={'inter-regular'}
+                      size={normalize(13)}
+                      color={colors.darkGray}>
+                      {appConstant.forgotPassword}
+                    </FontText>
+                  </View>
+                  <Button
+                    onPress={() => handleSubmit()}
+                    bgColor={colors.primary}
+                    style={s.loginBtn}
+                    buttonHeight={wp(14)}>
+                    <FontText
+                      name={'poppins-semibold'}
+                      size={normalize(16)}
+                      color={colors.white}>
+                      {appConstant.logIn}
+                    </FontText>
+                  </Button>
+                  </React.Fragment>
               )}
-            </View>
-            <View style={[s.textInputContainer, {marginTop: hp(-2)}]}>
-              <Input
-                withRightIcon
-                withTitle
-                ref={passwordRef}
-                title={appConstant.password}
-                titlecolor={colors.black}
-                titleSize={normalize(14)}
-                placeholder={'Enter Password'}
-                secureTextEntry={showPassword === false ? true : false}
-                value={password}
-                placeholderTextColor={colors.gray}
-                onChangeText={(text: any) => {
-                  setPassword(text.trim());
-                }}
-                keyboardType={'default'}
-                returnKeyType={'done'}
-                blurOnSubmit
-                onFocus={() => setPasswordFocus(!passwordFocus)}
-                onBlur={() => setPasswordFocus(!passwordFocus)}
-                inputStyle={[s.textInput]}
-                fontName={'poppins-medium'}
-                fontSize={normalize(14)}
-                color={'blue'}
-                style={{
-                  borderColor:
-                    passwordFocus === true
-                      ? colors.primary
-                      : colors.grayBorderColor,
-                  height: hp(6),
-                }}
-                rightIcon={
-                  <TouchableOpacity
-                    onPress={() => setShowPassword(!showPassword)}
-                    style={s.showIcon}>
-                    {showPassword === false ? (
-                      <SvgIcons.HideIcon />
-                    ) : (
-                      <SvgIcons.ShowIcon />
-                    )}
-                  </TouchableOpacity>
-                }
-              />
-              {passwordError === true && password.length < 6 ? (
-                <FontText color={colors.red} size={normalize(12)} pLeft={wp(2)}>
-                  {appConstant.passwordError}
-                </FontText>
-              ) : (
-                <FontText size={normalize(12)}> {''}</FontText>
-              )}
-            </View>
-            <View style={s.forgotPassContainer}>
-              <TouchableOpacity
-                style={s.rememberView}
-                onPress={() => setRememberPassword(!rememberPassword)}>
-                {rememberPassword === false ? (
-                  <SvgIcons.EmptyCheckbox />
-                ) : (
-                  <SvgIcons.CheckboxIcon />
-                )}
-                <FontText
-                  name={'inter-regular'}
-                  size={normalize(13)}
-                  color={colors.darkGray}
-                  pLeft={wp(2)}>
-                  {appConstant.rememberMe}
-                </FontText>
-              </TouchableOpacity>
-              <FontText
-                name={'inter-regular'}
-                size={normalize(13)}
-                color={colors.darkGray}>
-                {appConstant.forgotPassword}
-              </FontText>
-            </View>
-
-            <Button
-              onPress={() => onLoginPress()}
-              bgColor={colors.primary}
-              style={s.loginBtn}
-              buttonHeight={wp(14)}>
-              <FontText
-                name={'poppins-semibold'}
-                size={normalize(16)}
-                color={colors.white}>
-                {appConstant.logIn}
-              </FontText>
-            </Button>
+            </Formik>
             <View style={s.accountView}>
               <FontText
                 name={'inter-regular'}
@@ -216,26 +148,6 @@ const Login = ({navigation}: any) => {
                   pTop={hp(2)}>
                   {appConstant.signUp}
                 </FontText>
-              </TouchableOpacity>
-            </View>
-
-            <View style={s.dividerContainer}>
-              <View style={s.dividerLine} />
-              <Text style={s.dividerText}>{appConstant.orLogIn}</Text>
-              <View style={s.dividerLine} />
-            </View>
-
-            <View style={s.socialContainer}>
-              <TouchableOpacity>
-                <SvgIcons.FacebookIcon />
-              </TouchableOpacity>
-
-              <TouchableOpacity>
-                <SvgIcons.GoogleIcon />
-              </TouchableOpacity>
-
-              <TouchableOpacity>
-                <SvgIcons.AppleIcon />
               </TouchableOpacity>
             </View>
           </View>
